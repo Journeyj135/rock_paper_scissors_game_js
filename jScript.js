40034f05760d4ae8d5ee84a2480ed9a428f75e45
journey <newson1journey1@yahoo.com>
2026-01-27 12:19:38 -0800

    add function to get computers choice

--- jScript.js
+++ jScript.js
@@ -1,1 +1,24 @@
-console.log("Hello, World");
+// Create a function to get random numbers from 1-3 //
+
+function getRandomInt() {
+    const max = 3;
+    return Math.floor(Math.random() * max);
+}
+
+
+// Create a function to get the computers choice //
+
+function getComputerChoice() {
+
+    const randomNumber = getRandomInt();
+    
+    if(randomNumber === 0) {
+        return "Rock";
+    }else if(randomNumber === 1) {
+        return "Paper";
+    }else if(randomNumber === 2) {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
