0a508c3a0120b39f0c458cdceb43d58703a5279c
journey <newson1journey1@yahoo.com>
2026-01-27 12:31:46 -0800

    add function to prompt user for number

--- jScript.js
+++ jScript.js
@@ -1,5 +1,4 @@
 // Create a function to get random numbers from 1-3 //
-
 function getRandomInt() {
     const max = 3;
     return Math.floor(Math.random() * max);
@@ -7,7 +6,6 @@ function getRandomInt() {
 
 
 // Create a function to get the computers choice //
-
 function getComputerChoice() {
 
     const randomNumber = getRandomInt();
@@ -21,4 +19,18 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+// Create a function to get the humans choice //
+function getHumanChoice() {
+
+    const randomNumber = parseInt(prompt("Please Enter a Number between 0-2: "));
+    
+    if(randomNumber === 0) {
+        return "Rock";
+    }else if(randomNumber === 1) {
+        return "Paper";
+    }else if(randomNumber === 2) {
+        return "Scissors";
+    }
+}
+
+console.log(getHumanChoice());
